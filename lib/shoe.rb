93ea9924deccb13af1e5394799cace4aba9b0c84
Alexander Schelchere <ascellie@gmail.com>
2019-06-04 17:27:28 -0400

    shoe - complete

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,5 +1,16 @@
 class Shoe
-    def initialize
-        
+    def initialize(brand)
+        @brand = brand
+    end
+
+    attr_accessor :brand
+    attr_accessor :color
+    attr_accessor :size
+    attr_accessor :material
+    attr_accessor :condition
+
+    def cobble
+        puts "Your shoe is as good as new!"
+        @condition = "new"
     end
 end
