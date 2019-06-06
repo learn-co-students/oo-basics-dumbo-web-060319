667f48c1703fafbdf78450c9dae56ace3b0ac3eb
Abdul <awsayed@alaska.edu>
2019-06-06 05:45:24 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,44 @@
 
 
+class Book
+  def initialize(title)
+    @title = title
+  end
+
+  # Read the title
+  def title
+    @title
+  end
+
+  # write an author
+  def author=(author)
+    @author = author
+  end
+
+  # For the author to be accessable, you must provide it
+  def author
+    @author
+  end
+
+  def page_count=(page_count)
+    @page_count = page_count
+  end
+
+  def page_count
+    @page_count
+  end
+  
+    def genre=(genre)
+    @genre = genre
+  end
+
+  def genre
+    @genre
+  end
+  
+    def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+  
+end
+
